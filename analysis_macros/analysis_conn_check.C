064ceace550400ac4aad6acb1d3d42dc84cbfcc8
Maria <m.kondzielsk@student.uw.edu.pl>
2024-09-12 12:03:27 +0200

    optional suspicious channels

--- analysis_macros/analysis_conn_check.C
+++ analysis_macros/analysis_conn_check.C
@@ -23,6 +23,7 @@
 #include <fstream>
 #include <iomanip>
 #include <iostream>
+#include <optional>
 #include <sstream>
 #include <stdint.h>
 #include <stdlib.h>
@@ -30,6 +31,8 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <vector>
+// #include <thread>
+// #include <mutex>
 
 TString filename_data;
 std::vector<std::string> file_names;
@@ -37,6 +40,11 @@ int vcnt[128][32];
 // averages channel in the neighborhood for dynamic threshold
 std::vector<float> neigh_even;
 std::vector<float> neigh_odd;
+// weighted average based on Y projection of problematic or suspicious channels
+// based on the vicinity of a dynamic threshold
+std::optional<std::vector<Double_t>> sus_even;
+std::optional<std::vector<Double_t>> sus_odd;
+// std::mutex mtx;
 
 void analyze() {
   //! Variables for reading, analyzing data and displaying histograms
@@ -179,6 +187,30 @@ template <typename T> T calculate_median(std::vector<T> v, std::size_t n) {
   return (double)(v[(n - 1) / 2] + v[n / 2]) / 2.0;
 }
 
+Double_t calculate_weighted_av_ch(int ch, TH2F *h2) {
+  // histogram for keeping Y projection of h2hits_amp for this channel
+  // to transition from bins to channels, we always need to add 1
+  // since bins start from 1 but channels start from 0 we shift
+  TH1D *py_h2hits_amp = h2->ProjectionY("py_h2hits_amp", ch + 1, ch + 1, "");
+  // get the total sum of weights ing the Y projection
+  Double_t sum_of_weights_py_h2hits = py_h2hits_amp->Integral();
+  // initialize the weighted average sum of amplitude for this channel
+  Double_t sum_average_amp = 0.0;
+
+  for (Int_t n_bin = 1; n_bin < py_h2hits_amp->GetNbinsX(); n_bin++) {
+    Double_t weight_py_h2hits = py_h2hits_amp->GetBinContent(n_bin);
+    Double_t adc_value = py_h2hits_amp->GetXaxis()->GetBinLowEdge(n_bin);
+    sum_average_amp += weight_py_h2hits * adc_value;
+  }
+  delete py_h2hits_amp;
+  if (sum_of_weights_py_h2hits != 0) {
+    Double_t weighted_av_ch = sum_average_amp / sum_of_weights_py_h2hits;
+    return weighted_av_ch;
+  } else {
+    return sum_average_amp;
+  }
+}
+
 // Write a vector of channels to the histogram
 void fill_hist_with_ch(TH1F *hist, std::vector<int> channels, int *ch_hits) {
   if (channels.size() != 0) {
@@ -189,8 +221,9 @@ void fill_hist_with_ch(TH1F *hist, std::vector<int> channels, int *ch_hits) {
 }
 
 // ------------------------ Main function ---------------------
-int analysis_conn_check() {
+int analysis_conn_check(bool detect_sus_ch = true) {
 
+  // mtx.lock();
   for (int ch = 0; ch < 128; ch++) {
     for (int d = 0; d < 32; d++) {
       vcnt[ch][31 - d] = 0;
@@ -200,6 +233,8 @@ int analysis_conn_check() {
   TString dir = "";
   TString filename_root = "";
   std::vector<int> broken_channels;
+  std::optional<std::vector<int>> suspicious_channels;
+  std::optional<std::vector<int>> noisy_channels;
 
   // counter of broken channels accross all the files
   std::size_t n_broken_channels = 0;
@@ -208,6 +243,25 @@ int analysis_conn_check() {
   // number of channels in the neighborhood for dynamic threshold
   int n_neigh_ch = 25;
 
+  std::optional<float> sus_perc, n_sigmas;
+  /*sus_perc - percentage of threshold vicinity for determining suspicious
+   channels n_sigmas - accepted deviation from the median of weighted average of
+   suspicious channels' neighbors */
+  if (detect_sus_ch) {
+    sus_perc = 0.10;
+    n_sigmas = 4.5;
+    suspicious_channels = std::vector<int>();
+    noisy_channels = std::vector<int>();
+    sus_even = std::vector<Double_t>();
+    sus_odd = std::vector<Double_t>();
+  } else {
+    sus_perc.reset();
+    n_sigmas.reset();
+    suspicious_channels.reset();
+    noisy_channels.reset();
+    sus_even.reset();
+    sus_odd.reset();
+  }
   // reading file with list of measurements files.
   read_file_tests();
   // analyze function is called for every file in the list. At the end,
@@ -219,6 +273,10 @@ int analysis_conn_check() {
     analyze();
     // erase the previous channels
     broken_channels.clear();
+    if (detect_sus_ch) {
+      suspicious_channels.value().clear();
+      noisy_channels.value().clear();
+    }
 
     // Creating files
     TFile *file1 = new TFile(filename_root + ".root", "recreate");
@@ -241,6 +299,33 @@ int analysis_conn_check() {
     // histograms storing median values based on the neighboring channels
     TH1F h_med_even("h_med_even", "h_med_even", 128, 0, 128);
     TH1F h_med_odd("h_med_odd", "h_med_odd", 128, 0, 128);
+
+    // histogram storing suspicious channels
+    std::optional<TH1F> h_sus_ch;
+    // histogram storing extra noisy channels
+    std::optional<TH1F> h_noisy_ch;
+    // histogram for weighted average ADC of each channel
+    std::optional<TH1D> h_weighted_av;
+    // histogram for storing median of weighed average ADC of neighbors
+    std::optional<TH1D> h_med_weighted_av;
+    // histogram to get std dev of weighted average
+    std::optional<TH1D> h1_weighted_av_std;
+    if (detect_sus_ch) {
+      h_sus_ch.emplace("h_sus_ch", "h_sus_ch", 128, 0, 128);
+      h_noisy_ch.emplace("h_noisy_ch", "h_noisy_ch", 128, 0, 128);
+      h_weighted_av.emplace("h_weighted_av", "h_weighted_av", 128, 0, 128);
+      h_med_weighted_av.emplace("h_med_weighted_av", "h_med_weighted_av", 128,
+                                0, 128);
+      h1_weighted_av_std.emplace("h1_weighted_av_std", "h1_weighted_av_std",
+                                 100, 0, 15);
+
+    } else {
+      h_sus_ch.reset();
+      h_noisy_ch.reset();
+      h_weighted_av.reset();
+      h_med_weighted_av.reset();
+      h1_weighted_av_std.reset();
+    }
 
     // ---------------------------------------------- Histograms name
     h_ave_hits.SetTitle("Noise counts vs channel; Channel number; Counts");
@@ -274,6 +359,23 @@ int analysis_conn_check() {
         h_ave_odd.Fill(ch, ch_hits[ch]);
       }
     }
+
+    // stddev of weighted average
+    std::optional<Double_t> sus_neigh_sigma;
+    if (detect_sus_ch) {
+      // fill the weighted average hist with the value for current channel
+      for (int ch = 0; ch < 128; ch++) {
+        if (!std::isnan(calculate_weighted_av_ch(ch, &h2hits_amp))) {
+          h_weighted_av.value().Fill(ch,
+                                     calculate_weighted_av_ch(ch, &h2hits_amp));
+          h1_weighted_av_std.value().Fill(
+              calculate_weighted_av_ch(ch, &h2hits_amp));
+        }
+      }
+      sus_neigh_sigma = h1_weighted_av_std.value().GetStdDev();
+    } else {
+      sus_neigh_sigma.reset();
+    }
     // medians of neigh_even, neigh_odd vectors
     float neigh_med_even, neigh_med_odd;
     // dynamic thresholds for even and odd channels based on the neighborhood
@@ -285,6 +387,11 @@ int analysis_conn_check() {
       // erase the neighbors of the previous channel
       neigh_odd.clear();
       neigh_even.clear();
+      if (detect_sus_ch) {
+        // erase the weighted sums of surroundings of the previous channel
+        sus_even.value().clear();
+        sus_odd.value().clear();
+      }
 
       // the channel may not have enough neighbors to the left or right
       int start_ch = std::max(0, ch - n_neigh_ch);
@@ -299,6 +406,11 @@ int analysis_conn_check() {
           if (i != ch) {
 
             neigh_even.push_back(ch_hits[i]);
+            if (detect_sus_ch) {
+              // add weighted sum of ADC in neighboring channels
+              sus_even.value().push_back(
+                  calculate_weighted_av_ch(i, &h2hits_amp));
+            }
           }
         }
 
@@ -308,6 +420,18 @@ int analysis_conn_check() {
         thr_even = z_alpha * neigh_med_even;
         h_thr_even.Fill(ch, thr_even);
 
+        if (detect_sus_ch) {
+          // calculate the median of weighted sum in neighboring channels
+          h_med_weighted_av.value().Fill(
+              ch, calculate_median(sus_even.value(), sus_even->size()));
+
+          if (ch_hits[ch] > thr_even * n_sigmas.value()) {
+            // if the channel is extra noisy it's also masked
+            std::cout << "noisy" << '\n';
+
+            noisy_channels.value().push_back(ch);
+          }
+        }
         if (ch_hits[ch] < thr_even) {
           // if the channel is broken based on the dynamic threshold
 
@@ -318,6 +442,29 @@ int analysis_conn_check() {
             std::cout << "broken dynamic threshold NAR channel " << ch << '\n';
           } else {
             std::cout << "broken dynamic threshold channel " << ch << '\n';
+          }
+        } else if (detect_sus_ch && std::abs(ch_hits[ch] - thr_even) <
+                                        thr_even * sus_perc.value()) {
+          std::cout << "sus" << '\n';
+          // if the channel is suspicious based on the vicinity of threshold
+          // criteria separate for each channel
+          // remembering about the ch + 1 to bin shift
+          if (std::abs(h_weighted_av.value().GetBinContent(ch + 1) -
+                       h_med_weighted_av.value().GetBinContent(ch + 1)) >
+              n_sigmas.value() * sus_neigh_sigma.value()) {
+            // if the median of ADC weighted sum in neighboring channels is
+            // significantly different than for the suspicious channel
+            suspicious_channels.value().push_back(ch);
+            broken_channels.push_back(ch);
+
+            if (h_ave_hits.GetBinContent(ch + 1) == 0) {
+              //          // No analog response (NAR): broken channel
+              std::cout << "broken sus NAR channel " << ch << '\n';
+            } else {
+              std::cout << "broken sus channel " << ch << '\n';
+            }
+          } else {
+            suspicious_channels.value().push_back(ch);
           }
         }
       }
@@ -331,6 +478,10 @@ int analysis_conn_check() {
           if (i != ch) {
 
             neigh_odd.push_back(ch_hits[i]);
+            if (detect_sus_ch) {
+              sus_odd.value().push_back(
+                  calculate_weighted_av_ch(i, &h2hits_amp));
+            }
           }
         }
 
@@ -339,6 +490,19 @@ int analysis_conn_check() {
         h_med_odd.Fill(ch, neigh_med_odd);
         thr_odd = z_alpha * neigh_med_odd;
         h_thr_odd.Fill(ch, thr_odd);
+
+        if (detect_sus_ch) {
+          // calculate the median of weighted sum in neighboring channels
+          h_med_weighted_av.value().Fill(
+              ch, calculate_median(sus_odd.value(), sus_odd.value().size()));
+
+          if (ch_hits[ch] > thr_odd * n_sigmas.value()) {
+            // if the channel is extra noisy it's also masked
+            std::cout << "noisy" << '\n';
+
+            noisy_channels.value().push_back(ch);
+          }
+        }
 
         if (ch_hits[ch] < thr_odd) {
           // if the channel is broken based on the dynamic threshold
@@ -350,12 +514,41 @@ int analysis_conn_check() {
           } else {
             std::cout << "broken dynamic threshold channel " << ch << '\n';
           }
+        } else if (detect_sus_ch && std::abs(ch_hits[ch] - thr_odd) <
+                                        thr_odd * sus_perc.value()) {
+          std::cout << "sus" << '\n';
+
+          // if the channel is suspicious based on the vicinity of threshold
+          // criteria separate for each channel
+          //___Last criterium to find broken channels___
+          // remembering about the ch + 1 to bin shift
+          if (std::abs(h_weighted_av.value().GetBinContent(ch + 1) -
+                       h_med_weighted_av.value().GetBinContent(ch + 1)) >
+              n_sigmas.value() * sus_neigh_sigma.value()) {
+            // if the median of ADC weighted sum in neighboring channels if
+            // significantly different
+            suspicious_channels.value().push_back(ch);
+            broken_channels.push_back(ch);
+            if (h_ave_hits.GetBinContent(ch + 1) == 0) {
+              // No analog response (NAR): broken channel
+              std::cout << "broken sus NAR channel " << ch << '\n';
+            } else {
+              std::cout << "broken sus channel " << ch << '\n';
+            }
+          } else {
+            suspicious_channels.value().push_back(ch);
+          }
         }
       }
     }
 
     // Writing the histogram of the broken, suspicious and noisy channels
     fill_hist_with_ch(&h_broken_ch, broken_channels, ch_hits);
+    if (detect_sus_ch) {
+      fill_hist_with_ch(&h_sus_ch.value(), suspicious_channels.value(),
+                        ch_hits);
+      fill_hist_with_ch(&h_noisy_ch.value(), noisy_channels.value(), ch_hits);
+    }
 
     TCanvas *c1 = new TCanvas("c1", "c1", 900, 600);
 
@@ -372,6 +565,20 @@ int analysis_conn_check() {
     h_broken_ch.SetMarkerSize(1.5);
     h_broken_ch.SetMarkerStyle(23);
     h_broken_ch.SetMarkerColor(kBlack);
+
+    if (detect_sus_ch) {
+      // Adding suspicious channels
+      h_sus_ch.value().Draw("PHISTSAME");
+      h_sus_ch.value().SetMarkerSize(1.5);
+      h_sus_ch.value().SetMarkerStyle(22);
+      h_sus_ch.value().SetMarkerColor(kAzure + 2);
+
+      // Adding extra noisy channels
+      h_noisy_ch.value().Draw("PHISTSAME");
+      h_noisy_ch.value().SetMarkerSize(2);
+      h_noisy_ch.value().SetMarkerStyle(33);
+      h_noisy_ch.value().SetMarkerColor(kOrange + 1);
+    }
 
     h_med_even.Draw("PHISTSAME");
     h_med_even.SetLineWidth(0);
@@ -397,6 +604,12 @@ int analysis_conn_check() {
     lg_brk_summ->SetHeader("Connectivity check");
     lg_brk_summ->AddEntry(&h_ave_hits, "Noise hits", "l");
     lg_brk_summ->AddEntry(&h_broken_ch, "Broken channels", "p");
+    if (detect_sus_ch) {
+      if (suspicious_channels->size() != 0)
+        lg_brk_summ->AddEntry(&h_sus_ch.value(), "Suspicious channels", "p");
+      if (noisy_channels->size() != 0)
+        lg_brk_summ->AddEntry(&h_noisy_ch.value(), "Noisy channels", "p");
+    }
     lg_brk_summ->AddEntry(&h_med_even, "Median of neighboring even channels",
                           "p");
     lg_brk_summ->AddEntry(&h_med_odd, "Median of neighboring odd channels",
@@ -422,6 +635,12 @@ int analysis_conn_check() {
 
     h_ave_even.Write();
     h_ave_odd.Write();
+    if (detect_sus_ch) {
+      // add weighted average, its stddev and median
+      h_weighted_av.value().Write();
+      h_med_weighted_av.value().Write();
+      h1_weighted_av_std.value().Write();
+    }
 
     // close the opened root file
     file1->Close();
@@ -432,6 +651,16 @@ int analysis_conn_check() {
     delete c1;
     delete lg_brk_summ;
   }
-  write_parameters(z_alpha, n_neigh_ch, n_broken_channels);
+  // write_parameters(z_alpha, n_neigh_ch, n_broken_channels);
+  // mtx.unlock();
   return 0;
 }
+
+// int main(int argc, char *argv[]) {
+//   analysis_conn_check(true);
+// // std::thread thread_1(analysis_conn_check, true);
+// // // std::thread thread_2(analysis_conn_check, false);
+// // thread_1.join();
+// // // thread_2.join();
+// return 0;
+// }
